141a66f91fd7e84c9cfb867087fd61f6ef8a4ec1
Andrew Whatson <whatson@gmail.com>
2020-02-19 15:45:45 +1000

    Revised guile3.0-sdl2 package
    - building on guile3.0-gnutls was slow and shouldn't be necessary
    - instead, we only replace guile with guile-next at the top level

--- guile3.0-sdl2.scm
+++ guile3.0-sdl2.scm
@@ -1,14 +1,11 @@
 (use-modules (guix packages)
              (gnu packages guile)
-             (gnu packages sdl)
-             (gnu packages tls))
+             (gnu packages sdl))
 
-(define guile-variant-package-name
-  (@@ (gnu packages guile) guile-variant-package-name))
-
-(define package-for-guile-3.0
-  (package-input-rewriting `((,guile-2.2 . ,guile-next)
-                             (,gnutls    . ,guile3.0-gnutls))
-                           (guile-variant-package-name "guile3.0")))
-
-(package-for-guile-3.0 guile-sdl2)
+(package
+  (inherit guile-sdl2)
+  (name "guile3.0-sdl2")
+  (native-inputs
+    (assoc-set!
+      (package-native-inputs guile-sdl2)
+      "guile" `(,guile-next))))
