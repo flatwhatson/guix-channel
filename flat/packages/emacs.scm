07e9f4f973d57285d4f57efa9d51baf098caf1e7
Andrew Whatson <whatson@gmail.com>
2020-05-16 23:54:04 +1000

    Update, include guix patches from emacs/emacs-next

--- flat/packages/emacs.scm
+++ flat/packages/emacs.scm
@@ -8,11 +8,8 @@
   #:use-module (gnu packages emacs)
   #:use-module (gnu packages gcc))
 
-;; NOTE: This recipe intentionally leaves out guix-specific patches from the
-;; built-in emacs recipes, in an attempt to produce a "vanilla" emacs suitable
-;; for shipping with "guix pack".
 (define-public emacs-native-comp
-  (let ((commit "92dc81f85e1b91db04487ccf1b52c0cd3328dfee")
+  (let ((commit "d6f6353cfdbbea5501915675081265b4dc4591e3")
         (revision "0")
         (emacs-version "28.0.50"))
     (package
@@ -26,28 +23,17 @@
              (url "https://git.savannah.gnu.org/git/emacs.git")
              (commit commit)))
        (sha256
-        (base32 "1f22bxwq53hhdjlakmqz66y63vix5ybpnc1pk9fpy18wjh871scq"))
+        (base32 "132cfhzl8lj0yxk05gkxb3i7wjn4ar4bkv82cw71300mzmd0p1sn"))
        (file-name (git-file-name name version))
-       (patches (search-patches "emacs27-exec-path.patch"
-                                "emacs-source-date-epoch.patch"))))
+       (patches (origin-patches (package-source emacs-next)))
+       (modules (origin-modules (package-source emacs-next)))
+       (snippet (origin-snippet (package-source emacs-next)))))
      (arguments
       (substitute-keyword-arguments (package-arguments emacs-next)
        ((#:configure-flags flags)
         `(cons* "--with-nativecomp" ,flags))
        ((#:phases phases)
-        `(modify-phases %standard-phases
-           ;; Remove hard-coded /bin/pwd from the Makefiles.
-           (add-before 'configure 'fix-/bin/pwd
-             (lambda _
-               (substitute* (find-files "." "^Makefile\\.in$")
-                            (("/bin/pwd") "pwd"))
-               #t))
-           ;; Make the gzip files writable so guix can set timestamps.
-           (add-before 'reset-gzip-timestamps 'make-compressed-files-writable
-             (lambda _
-               (for-each make-file-writable
-                         (find-files %output ".*\\.t?gz$"))
-               #t))
+        `(modify-phases ,phases
            ;; Add build-time library paths for libgccjit.
            (add-before 'configure 'set-libgccjit-path
              (lambda* (#:key inputs #:allow-other-keys)
@@ -60,7 +46,7 @@
                                         (getenv "LIBRARY_PATH"))))
                #t))
            ;; Add runtime library paths for libgccjit,
-           (add-after 'glib-or-gtk-wrap 'wrap-library-path
+           (add-before 'restore-emacs-pdmp 'wrap-library-path
              (lambda* (#:key inputs outputs #:allow-other-keys)
                (let* ((gcc-libdir
                        (string-append (assoc-ref inputs "gcc:lib")
@@ -86,22 +72,18 @@
                                 program
                                 `("LIBRARY_PATH" prefix ,library-path))))
                            bin-list))
-               #t))
-           ;; Emacs' dumper files are incorrectly detected as executables, and
-           ;; get wrapped in a launcher script.  Move the originals back.
-           (add-after 'wrap-library-path 'restore-emacs-pdmp
-             (lambda* (#:key outputs target #:allow-other-keys)
-               (let* ((output    (assoc-ref outputs "out"))
-                      (libexec   (string-append output "/libexec"))
-                      (pdmp      (find-files libexec "^emacs\\.pdmp$"))
-                      (pdmp-real (find-files libexec "^\\.emacs\\.pdmp-real$")))
-                 (for-each (lambda (wrapper real)
-                             (delete-file wrapper)
-                             (rename-file real wrapper))
-                           pdmp pdmp-real))
                #t))))))
      (inputs
       `(("gcc:lib" ,gcc "lib")
         ("glibc" ,glibc)
         ("libgccjit" ,libgccjit)
-        ,@(package-inputs emacs-next))))))
+        ,@(package-inputs emacs-next)))
+     (native-search-paths
+      (list (search-path-specification
+             (variable "EMACSLOADPATH")
+             (files
+              (list "share/emacs/site-lisp"
+                    (string-append "share/emacs/" emacs-version "/lisp"))))
+            (search-path-specification
+             (variable "INFOPATH")
+             (files '("share/info"))))))))
