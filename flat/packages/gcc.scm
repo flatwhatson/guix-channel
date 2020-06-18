07d1ddbc354919c2d236cdba55adf88c78aecf9e
Andrew Whatson <whatson@gmail.com>
2020-06-18 12:02:51 +1000

    Factor out libgccjit package modifier

--- flat/packages/gcc.scm
+++ flat/packages/gcc.scm
@@ -18,34 +18,39 @@
 
 (define-module (flat packages gcc)
   #:use-module (guix packages)
+  #:use-module (guix memoization)
   #:use-module (guix build utils)
   #:use-module (guix utils)
   #:use-module (gnu packages)
   #:use-module (gnu packages gcc))
 
+(define-public libgccjit-for-gcc
+  (mlambda (gcc)
+    (package
+      (inherit gcc)
+      (name "libgccjit")
+      (outputs (delete "lib" (package-outputs gcc)))
+      (properties (alist-delete 'hidden? (package-properties gcc)))
+      (arguments
+       (substitute-keyword-arguments `(#:modules ((guix build gnu-build-system)
+                                                  (guix build utils)
+                                                  (ice-9 regex)
+                                                  (srfi srfi-1)
+                                                  (srfi srfi-26))
+                                       ,@(package-arguments gcc))
+         ((#:configure-flags flags)
+          `(append `("--enable-host-shared"
+                     ,(string-append "--enable-languages=jit"))
+                   (remove (cut string-match "--enable-languages.*" <>)
+                           ,flags)))
+         ((#:phases phases)
+          `(modify-phases ,phases
+             (add-after 'install 'remove-broken-or-conflicting-files
+               (lambda* (#:key outputs #:allow-other-keys)
+                 (for-each delete-file
+                           (find-files (string-append (assoc-ref outputs "out") "/bin")
+                                       ".*(c\\+\\+|cpp|g\\+\\+|gcov|gcc|gcc-.*)"))
+                 #t)))))))))
+
 (define-public libgccjit-10
-  (package
-    (inherit gcc-10)
-    (name "libgccjit")
-    (outputs (delete "lib" (package-outputs gcc)))
-    (properties (alist-delete 'hidden? (package-properties gcc)))
-    (arguments
-     (substitute-keyword-arguments `(#:modules ((guix build gnu-build-system)
-                                                (guix build utils)
-                                                (ice-9 regex)
-                                                (srfi srfi-1)
-                                                (srfi srfi-26))
-                                     ,@(package-arguments gcc))
-       ((#:configure-flags flags)
-        `(append `("--enable-host-shared"
-                   ,(string-append "--enable-languages=jit"))
-                 (remove (cut string-match "--enable-languages.*" <>)
-                         ,flags)))
-       ((#:phases phases)
-        `(modify-phases ,phases
-           (add-after 'install 'remove-broken-or-conflicting-files
-             (lambda* (#:key outputs #:allow-other-keys)
-               (for-each delete-file
-                         (find-files (string-append (assoc-ref outputs "out") "/bin")
-                                     ".*(c\\+\\+|cpp|g\\+\\+|gcov|gcc|gcc-.*)"))
-               #t))))))))
+  (libgccjit-for-gcc gcc-10))
