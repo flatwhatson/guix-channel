39d4d91225da4570af62a69aa523942789640fa2
Andrew Whatson <whatson@gmail.com>
2020-06-18 11:50:59 +1000

    Factor out emacs package modifiers

--- flat/packages/emacs.scm
+++ flat/packages/emacs.scm
@@ -28,15 +28,84 @@
   #:use-module (gnu packages xorg)
   #:use-module (flat packages gcc))
 
+(define (emacs-with-native-comp emacs gcc libgccjit)
+  (package
+    (inherit emacs)
+    (arguments
+     (substitute-keyword-arguments (package-arguments emacs)
+       ((#:configure-flags flags)
+        `(cons* "--with-nativecomp" ,flags))
+       ((#:phases phases)
+        `(modify-phases ,phases
+           ;; Add build-time library paths for libgccjit.
+           (add-before 'configure 'set-libgccjit-path
+             (lambda* (#:key inputs #:allow-other-keys)
+               (let ((libgccjit-libdir
+                      (string-append (assoc-ref inputs "libgccjit")
+                                     "/lib/gcc/" %host-type "/"
+                                     ,(package-version libgccjit) "/")))
+                 (setenv "LIBRARY_PATH"
+                         (string-append libgccjit-libdir ":"
+                                        (getenv "LIBRARY_PATH"))))
+               #t))
+           ;; Add runtime library paths for libgccjit,
+           (add-before 'restore-emacs-pdmp 'wrap-library-path
+             (lambda* (#:key inputs outputs #:allow-other-keys)
+               (let* ((gcc-libdir
+                       (string-append (assoc-ref inputs "gcc:lib")
+                                      "/lib/"))
+                      (glibc-libdir
+                       (string-append (assoc-ref inputs "glibc")
+                                      "/lib/"))
+                      (libgccjit-libdir
+                       (string-append (assoc-ref inputs "libgccjit")
+                                      "/lib/gcc/" %host-type "/"
+                                      ,(package-version libgccjit) "/"))
+                      (library-path (list gcc-libdir
+                                          glibc-libdir
+                                          libgccjit-libdir))
+                      (output   (assoc-ref outputs "out"))
+                      (bindir   (string-append output "/bin"))
+                      (libexec  (string-append output "/libexec"))
+                      (bin-list (append (find-files bindir ".*")
+                                        (find-files libexec ".*"))))
+                 (for-each (lambda (program)
+                             (unless (wrapper? program)
+                               (wrap-program
+                                   program
+                                 `("LIBRARY_PATH" prefix ,library-path))))
+                           bin-list))
+               #t))))))
+    (native-inputs
+     `(("gcc" ,gcc)
+       ,@(package-native-inputs emacs)))
+    (inputs
+     `(("gcc:lib" ,gcc "lib")
+       ("glibc" ,glibc)
+       ("libgccjit" ,libgccjit)
+       ,@(package-inputs emacs)))))
+
+(define (emacs-with-xwidgets emacs)
+  (package
+    (inherit emacs)
+    (arguments
+     (substitute-keyword-arguments (package-arguments emacs)
+       ((#:configure-flags flags)
+        `(cons* "--with-xwidgets" ,flags))))
+    (inputs
+     `(("libxcomposite" ,libxcomposite)
+       ("webkitgtk" ,webkitgtk)
+       ,@(package-inputs emacs)))))
+
 (define-public emacs-native-comp
-  (let ((commit "5a55a845a7c426e82e8a6a6d02bc4a39992871e3")
-        (checksum "134zn4jd96flz7n5j30k62sg876pi3awz1wjlmndv6crq961ibf8")
-        (revision "0")
-        (emacs-version "28.0.50")
-        (gcc gcc-10)
-        (libgccjit libgccjit-10))
+  (let* ((commit "5a55a845a7c426e82e8a6a6d02bc4a39992871e3")
+         (checksum "134zn4jd96flz7n5j30k62sg876pi3awz1wjlmndv6crq961ibf8")
+         (revision "0")
+         (emacs-version "28.0.50")
+         (emacs (emacs-with-native-comp emacs-next gcc-10 libgccjit-10))
+         (emacs (emacs-with-xwidgets emacs)))
     (package
-      (inherit emacs-next)
+      (inherit emacs)
       (name "emacs-native-comp")
       (version (git-version emacs-version revision commit))
       (source
@@ -47,66 +116,9 @@
                (commit commit)))
          (sha256 (base32 checksum))
          (file-name (git-file-name name version))
-         (patches (origin-patches (package-source emacs-next)))
-         (modules (origin-modules (package-source emacs-next)))
-         (snippet (origin-snippet (package-source emacs-next)))))
-      (arguments
-       (substitute-keyword-arguments (package-arguments emacs-next)
-         ((#:configure-flags flags)
-          `(cons* "--with-nativecomp"
-                  "--with-xwidgets"
-                  ,flags))
-         ((#:phases phases)
-          `(modify-phases ,phases
-             ;; Add build-time library paths for libgccjit.
-             (add-before 'configure 'set-libgccjit-path
-               (lambda* (#:key inputs #:allow-other-keys)
-                 (let ((libgccjit-libdir
-                        (string-append (assoc-ref inputs "libgccjit")
-                                       "/lib/gcc/" %host-type "/"
-                                       ,(package-version libgccjit) "/")))
-                   (setenv "LIBRARY_PATH"
-                           (string-append libgccjit-libdir ":"
-                                          (getenv "LIBRARY_PATH"))))
-                 #t))
-             ;; Add runtime library paths for libgccjit,
-             (add-before 'restore-emacs-pdmp 'wrap-library-path
-               (lambda* (#:key inputs outputs #:allow-other-keys)
-                 (let* ((gcc-libdir
-                         (string-append (assoc-ref inputs "gcc:lib")
-                                        "/lib/"))
-                        (glibc-libdir
-                         (string-append (assoc-ref inputs "glibc")
-                                        "/lib/"))
-                        (libgccjit-libdir
-                         (string-append (assoc-ref inputs "libgccjit")
-                                        "/lib/gcc/" %host-type "/"
-                                        ,(package-version libgccjit) "/"))
-                        (library-path (list gcc-libdir
-                                            glibc-libdir
-                                            libgccjit-libdir))
-                        (output   (assoc-ref outputs "out"))
-                        (bindir   (string-append output "/bin"))
-                        (libexec  (string-append output "/libexec"))
-                        (bin-list (append (find-files bindir ".*")
-                                          (find-files libexec ".*"))))
-                   (for-each (lambda (program)
-                               (unless (wrapper? program)
-                                 (wrap-program
-                                     program
-                                   `("LIBRARY_PATH" prefix ,library-path))))
-                             bin-list))
-                 #t))))))
-      (native-inputs
-       `(("gcc" ,gcc)
-         ,@(package-native-inputs emacs-next)))
-      (inputs
-       `(("gcc:lib" ,gcc "lib")
-         ("glibc" ,glibc)
-         ("libgccjit" ,libgccjit)
-         ("libxcomposite" ,libxcomposite)
-         ("webkitgtk" ,webkitgtk)
-         ,@(package-inputs emacs-next)))
+         (patches (origin-patches (package-source emacs)))
+         (modules (origin-modules (package-source emacs)))
+         (snippet (origin-snippet (package-source emacs)))))
       (native-search-paths
        (list (search-path-specification
               (variable "EMACSLOADPATH")
